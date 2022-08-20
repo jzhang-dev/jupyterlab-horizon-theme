23ec2d7145c8fd2677b0678757b5917eddf1a130
Jia-Yuan Zhang <56027973+jzhang-dev@users.noreply.github.com>
2022-08-20 17:43:00 +0100

    Fix index.ts building
    Fix build

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,7 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import {
-  IThemeManager
-} from '@jupyterlab/apputils';
+import {IThemeManager} from '@jupyterlab/apputils';
 
 /**
  * A plugin for @mohirio/jupyterlab-horizon-theme
@@ -13,20 +11,20 @@ import {
 const plugin: JupyterFrontEndPlugin<void> = {
   id: '@mohirio/jupyterlab-horizon-theme:plugin',
   requires: [IThemeManager],
-  activate: function(app: JupyterFrontEnd, manager: IThemeManager) {
+  activate: function (app: JupyterFrontEnd, manager: IThemeManager) {
     manager.register({
       name: 'JupyterLab Horizon',
       isLight: false,
-      load: function() {
-        let meta: HTMLMetaElement = document.createElement("meta");
-        meta.name = "theme-color";
-        meta.id = "theme-color-horizon"
-        meta.content = "#1C1E26";
-        document.getElementsByTagName("head")[0].appendChild(meta);
+      load: function () {
+        let const: HTMLMetaElement = document.createElement('meta');
+        meta.name = 'theme-color';
+        meta.id = 'theme-color-horizon'
+        meta.content = '#1C1E26';
+        document.getElementsByTagName('head')[0].appendChild(meta);
         return manager.loadCSS('@mohirio/jupyterlab-horizon-theme/index.css');
       },
-      unload: function() {
-        let meta: HTMLElement = document.getElementById("theme-color-horizon");
+      unload: function () {
+        let const: HTMLElement = document.getElementById('theme-color-horizon');
         meta.parentNode.removeChild(meta);
         return Promise.resolve(void 0);
       }
